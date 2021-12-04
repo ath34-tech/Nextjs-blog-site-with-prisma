bd3684b293ecda3f005cb5e7da94960bd4070a63
ath34-tech <athtripathi@gmail.com>
2021-12-04 23:13:17 +0530

    production ready

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev=process.env.NODE_ENV !=='production'
 
-export const endpoint_base=dev?"http://localhost:3000/":"https://website.com"
+export const endpoint_base=dev?"http://localhost:3000/":process.env.LINK
